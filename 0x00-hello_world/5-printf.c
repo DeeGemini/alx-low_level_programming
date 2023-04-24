6499b2b15dea8dc3d51e3ba98ac50a96a0fabe55
DeeGemini <nicsadyngwenya@gmail.com>
2023-04-24 16:38:33 -0700

    Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Emtry points for the program
+ * This function prints aline using printf
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);
+}
