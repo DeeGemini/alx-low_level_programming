e949f880de7b2413d6da12003511bfd49d33ebc8
DeeGemini <zodiactheegemini@gmail.com>
2023-06-03 17:04:30 -0700

    2-args

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -13,13 +13,7 @@ int main(int argc, char *argv[])
 {
 	int i;
 
-	if (argc <= 1)
-	{
-		printf("No arguments provided.\n");
-		return (1);
-	}
-
-	for (i = 1; i < argc; i++)
+	for (i = 0; i < argc; i++)
 	{
 		printf("%s\n", argv[i]);
 	}
