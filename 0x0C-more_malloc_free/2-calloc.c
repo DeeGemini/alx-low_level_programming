d6d99e66758c94fa739ca24ba790d051fb0e3118
DeeGemini <zodiactheegemini@gmail.com>
2023-06-05 13:52:46 -0700

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,13 +1,33 @@
 #include <stdlib.h>
-#include <string.h>
 #include "main.h"
+
+/**
+ * *_memset - It fills memory with a constant byte
+ * @s: A memory area to be filled
+ * @b: A char to copy
+ * @n: The number of times to copy b
+ *
+ * Return: pointer to the memory area s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+
+	return (s);
+}
 
 /**
  * *_calloc - It allocates memory for an array
  * @nmemb: The number of elements in the array
  * @size: The size of each element
  *
- * Return: pointer to allocated memory
+ * Return: A pointer to allocated memory
  */
 
 void *_calloc(unsigned int nmemb, unsigned int size)
@@ -25,25 +45,4 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	_memset(ptr, 0, nmemb * size);
 
 	return (ptr);
-}
-
-/**
- * *_memset - It fills memory with a constant byte
- * @s: A memory area to be filled
- * @b: A char to copy
- * @n: Number of times to copy b
- *
- * Return: pointer to the memory area s
- */
-
-char *_memset(char *s, char b, unsigned int n)
-{
-	unsigned int i;
-
-	for (i = 0; i < n; i++)
-	{
-		s[i] = b;
-	}
-
-	return (s);
 }
